48c97462dc69a557854671f4f04734cec303afb3
Shivinai <dm.padnevich@gmail.com>
2026-05-16 16:43:01 +0300

    Initial commit

---
+++ dsptest.srcs/sources_1/new/Task1.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/16/2026 04:18:12 PM
+// Design Name: 
+// Module Name: Task1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Task1(
+
+    );
+endmodule
